48707470f136a75bdce6fb922830a21ebb48deaf
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-28 22:38:57 -0300

    feat: refactor Canvas component to CanvasModel and add Backdrop and CameraRig components

--- src/canvas/index.tsx
+++ src/canvas/index.tsx
@@ -1,4 +1,21 @@
-interface CanvasProps {}
-export default function Canvas(props: CanvasProps) {
-  return <></>
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
+import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
+import Shirt from './Shirt'
+
+export default function CanvasModel() {
+  return (
+    <Canvas shadows camera={{ position: [0, 0, 0], fov: 25 }} gl={{ preserveDrawingBuffer: true }} className='w-full max-w-full h-full transition-all ease-in'>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+
+      <CameraRig>
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  )
 }

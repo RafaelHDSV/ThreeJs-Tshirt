d25a8032c137eeb98750568de9a4a79ee6b9af3b
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-28 21:01:46 -0300

    feat: add CustomButton component with Valtio state management

---
+++ src/components/CustomButton/CustomButton.tsx
@@ -0,0 +1,27 @@
+import { useSnapshot } from 'valtio'
+import state from '../../store'
+
+interface ICustomButtonProps {
+  type: 'filled'
+  title: string
+  handleClick: () => void
+  customStyles: string
+}
+
+export default function CustomButton({ type, title, handleClick, customStyles }: ICustomButtonProps) {
+  const snap = useSnapshot(state)
+  const generateStyle = (type: string) => {
+    if (type === 'filled') {
+      return {
+        background: snap.color,
+        color: '#fff'
+      }
+    }
+  }
+
+  return (
+    <button className={`px-2 py-1.2 flex-1 rounded-md ${customStyles}`} style={generateStyle(type)} onClick={handleClick}>
+      {title}
+    </button>
+  )
+}

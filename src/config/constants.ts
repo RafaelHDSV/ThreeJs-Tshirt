3530031977d39518de6b4ae2df1ce7ac649fbe30
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-29 21:33:17 -0300

    fix: update asset imports in constants file for consistency and clarity

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,38 +1,42 @@
-import { swatch, fileIcon, ai, logoShirt, stylishShirt } from "../assets";
+import swatch from '../assets/swatch.png'
+import fileIcon from '../assets/file.png'
+import ai from '../assets/ai.png'
+import logoShirt from '../assets/logo-tshirt.png'
+import stylishShirt from '../assets/stylish-tshirt.png'
 
 export const EditorTabs = [
   {
-    name: "colorpicker",
-    icon: swatch,
+    name: 'colorpicker',
+    icon: swatch
   },
   {
-    name: "filepicker",
-    icon: fileIcon,
+    name: 'filepicker',
+    icon: fileIcon
   },
   {
-    name: "aipicker",
-    icon: ai,
-  },
-];
+    name: 'aipicker',
+    icon: ai
+  }
+]
 
 export const FilterTabs = [
   {
-    name: "logoShirt",
-    icon: logoShirt,
+    name: 'logoShirt',
+    icon: logoShirt
   },
   {
-    name: "stylishShirt",
-    icon: stylishShirt,
-  },
-];
+    name: 'stylishShirt',
+    icon: stylishShirt
+  }
+]
 
 export const DecalTypes = {
   logo: {
-    stateProperty: "logoDecal",
-    filterTab: "logoShirt",
+    stateProperty: 'logoDecal',
+    filterTab: 'logoShirt'
   },
   full: {
-    stateProperty: "fullDecal",
-    filterTab: "stylishShirt",
-  },
-};
+    stateProperty: 'fullDecal',
+    filterTab: 'stylishShirt'
+  }
+}

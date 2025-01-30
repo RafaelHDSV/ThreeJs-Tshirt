333098942b503c85fc3de2905c268d3c3e1420d8
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-29 21:33:09 -0300

    feat: enhance Tab component with improved props structure and logging

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,4 +1,20 @@
-interface TabProps {}
-export default function Tab(props: TabProps) {
-  return <>tab</>
+interface ITabProps {
+  key: number
+  tab: { name: string; icon: string }
+  handleClick: () => void
+  isFilterTab?: boolean
+  isActiveTab?: string
+}
+export default function Tab({ key, tab, handleClick, isFilterTab, isActiveTab }: ITabProps) {
+  console.log('key', key)
+  console.log('tab', tab)
+  console.log('handleClick', handleClick)
+  console.log('isFilterTab', isFilterTab)
+  console.log('isActiveTab', isActiveTab)
+
+  return (
+    <>
+      <p>tab</p>
+    </>
+  )
 }

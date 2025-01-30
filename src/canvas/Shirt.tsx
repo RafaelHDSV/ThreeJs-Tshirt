aa511c6e414065eb4b63ed4ef09a93c3ee21c60e
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-29 21:32:58 -0300

    fix: update Shirt component to correct GLTF path and streamline useFrame logic

--- src/canvas/Shirt.tsx
+++ src/canvas/Shirt.tsx
@@ -7,22 +7,18 @@ import { easing } from 'maath'
 
 export default function Shirt() {
   const snap = useSnapshot(state)
-  const { nodes, materials } = useGLTF('./shirt_baked.glb')
-
-  console.log(nodes)
+  const { nodes, materials } = useGLTF('/shirt_baked.glb')
 
   const logoTexture = useTexture(snap.logoDecal)
   const fullTexture = useTexture(snap.fullDecal)
 
-  useFrame((state, delta) => {
-    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
-  })
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta))
 
   const stateString = JSON.stringify(snap)
 
   return (
     <group key={stateString}>
-      <mesh castShadow geometry={(nodes.T_Shirt_male as Mesh).geometry} material={materials.lambert1} material-roughness={1} dispose={null}>
+      <mesh castShadow geometry={nodes.T_Shirt_male.geometry} material={materials.lambert1} material-roughness={1} dispose={null}>
         {snap.isFullTexture && <Decal position={[0, 0, 0]} rotation={[0, 0, 0]} scale={1} map={fullTexture} />}
         {snap.isLogoTexture && (
           <Decal

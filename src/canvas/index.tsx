09fadf5f6a1db353c0eea4a844caf36f93e0d1fb
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-30 08:41:58 -0300

    fix: reorder imports and refactor CanvasModel component for clarity

--- src/canvas/index.tsx
+++ src/canvas/index.tsx
@@ -1,13 +1,13 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei'
-import CameraRig from './CameraRig'
-import Backdrop from './Backdrop'
 import Shirt from './Shirt'
+import Backdrop from './Backdrop'
+import CameraRig from './CameraRig'
 
-export default function CanvasModel() {
+const CanvasModel = () => {
   return (
     <Canvas shadows camera={{ position: [0, 0, 0], fov: 25 }} gl={{ preserveDrawingBuffer: true }} className='w-full max-w-full h-full transition-all ease-in'>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.5 * Math.PI} />
       <Environment preset='city' />
 
       <CameraRig>
@@ -19,3 +19,5 @@ export default function CanvasModel() {
     </Canvas>
   )
 }
+
+export default CanvasModel

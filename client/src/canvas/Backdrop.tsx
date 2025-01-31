c862f0b168464596f7c47997dc908454c5225ee3
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-31 07:09:02 -0300

    feat: enhance Backdrop component with type definitions and improved color handling

--- client/src/canvas/Backdrop.tsx
+++ client/src/canvas/Backdrop.tsx
@@ -1,9 +1,32 @@
+import * as THREE from 'three'
 import { useRef } from 'react'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
+import { ReactThreeFiber, useFrame } from '@react-three/fiber'
+import { easing } from 'maath'
+
+interface AccumulativeContext {
+  lights: Map<THREE.Light, THREE.Object3D>
+  temporal: boolean
+  frames: number
+  blend: number
+  count: number
+  getMesh: () => THREE.Mesh<
+    THREE.PlaneGeometry,
+    { map: THREE.Texture; color?: ReactThreeFiber.Color; alphaTest?: number; blend?: number } & THREE.ShaderMaterial
+  >
+  reset: () => void
+  update: (frames?: number) => void
+}
 
 export default function Backdrop() {
-  const shadows = useRef(null)
-  // useFrame((state, delta) => easing.dampC(shadows.current?.getMesh().material.color, state.color, 0.25, delta))
+  const shadows = useRef<AccumulativeContext | null>(null)
+  useFrame((state: THREE.Color, delta) => {
+    const color = shadows.current?.getMesh().material.color as THREE.Color
+    const targetColor = state.color as THREE.Color
+    if (color) {
+      easing.dampC(color, targetColor, 0.25, delta)
+    }
+  })
 
   return (
     <AccumulativeShadows ref={shadows} temporal frames={60} alphaTest={0.85} scale={5} rotation={[Math.PI / 2, 0, 0]} position={[0, 0, -0.14]}>

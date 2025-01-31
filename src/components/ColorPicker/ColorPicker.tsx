c07da96448895e7b3f63887d1ff449d84626f4fd
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-30 21:15:39 -0300

    feat: implement ColorPicker component with color selection functionality using SketchPicker

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -1,4 +1,18 @@
-interface ColorPickerProps {}
-export default function ColorPicker(props: ColorPickerProps) {
-  return <></>
+import { useSnapshot } from 'valtio'
+import state from '../../store'
+import { SketchPicker } from 'react-color'
+
+export default function ColorPicker() {
+  const snap = useSnapshot(state)
+
+  return (
+    <div className='absolute left-full ml-3'>
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={color => (state.color = color.hex)}
+        presetColors={['#383938', '#A0A3A0', '#8D0000', '#008B00', '#161684', '#878702', '#008181', '#950B95']}
+      />
+    </div>
+  )
 }

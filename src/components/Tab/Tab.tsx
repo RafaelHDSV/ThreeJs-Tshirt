6fc9e88791285cb4458c631e7a246be34757b53a
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-30 21:15:16 -0300

    fix: remove unused props and enhance Tab component with state-based styling

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,20 +1,19 @@
+import { useSnapshot } from 'valtio'
+import state from '../../store'
+
 interface ITabProps {
-  key: number
   tab: { name: string; icon: string }
   handleClick: () => void
   isFilterTab?: boolean
   isActiveTab?: string
 }
-export default function Tab({ key, tab, handleClick, isFilterTab, isActiveTab }: ITabProps) {
-  console.log('key', key)
-  console.log('tab', tab)
-  console.log('handleClick', handleClick)
-  console.log('isFilterTab', isFilterTab)
-  console.log('isActiveTab', isActiveTab)
+export default function Tab({ tab, handleClick, isFilterTab, isActiveTab }: ITabProps) {
+  const snap = useSnapshot(state)
+  const activeStyles = isFilterTab && isActiveTab ? { backgroundColor: snap.color, opacity: 0.5 } : { backgroundColor: 'transparent', opacity: 1 }
 
   return (
-    <>
-      <p>tab</p>
-    </>
+    <div className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'}`} onClick={handleClick} style={activeStyles}>
+      <img className={isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'} src={tab.icon} alt={tab.name} />
+    </div>
   )
 }
